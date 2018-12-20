d3d70d61cc7349f355ec7c442a841ee36dc8e668
Viachaslau Tyshkavets <pmstss@gmail.com>
2018-12-20 15:27:17 +0300

    robots.txt fix

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(compression({ threshold: 0 }));
 
 app.use(express.static(__dirname + '/dist/omsprofit'));
 
+app.get('/robots.txt', (req, res) => {
+    res.sendFile(path.join(__dirname + '/robots.txt'));
+});
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/omsprofit/index.html'));
 });
